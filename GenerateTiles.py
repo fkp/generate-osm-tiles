af0e58bdd88c314dd9fe09ae1e47aa5665a575b3
fkp <fraser.a.kirkpatrick@gmail.com>
2015-04-25 22:49:13 +0100

    Added better support for killing script halfway through

--- GenerateTiles.py
+++ GenerateTiles.py
@@ -4,6 +4,8 @@ from osgeo import ogr
 import sys, os, threading, time, math, argparse
 from Queue import *
 
+shutdownRequested = False
+
 class TileParams:
     def __init__(self, inMinX, inMinY, inMaxX, inMaxY, inTileSize, inZoom, inPad):
         ring = ogr.Geometry(ogr.wkbLinearRing)
@@ -50,7 +52,7 @@ class ThreadedTileGenerator(threading.Thread):
         self.scale = inScale
 
     def run(self):
-        while True:
+        while not shutdownRequested:
 
             # Blocks, waiting for something to become avialable on the queue. This at the moment can give a NoneType error while the script is in the process of shutting down. Can't figure out how to stop this but it doesn't do any harm as all the queued items are done and the script is terminating.
             item = self.queue.get()
@@ -83,7 +85,8 @@ class ThreadedTileGenerator(threading.Thread):
             m.zoom_to_box(bbox)
 
             # This prevents labels being drawn on the boundary of the tiles
-            m.buffer_size = params.GetTileSize() / 2
+            m.buffer_size = ( params.GetTileSize() / 2 )
+            self.Log("Using buffer size: " + str(m.buffer_size))
 
             im = Image(params.GetTileSize(),params.GetTileSize())
             render(m, im, self.scale)
@@ -100,99 +103,104 @@ class ThreadedTileGenerator(threading.Thread):
         # Prefix all log messages with the thread number and current queue size
         print "[t" + str(self.threadnum) + ", q" + str(self.queue.qsize()) + "] " + message
 
-parser = argparse.ArgumentParser(description='A script to generate OSM tiles in a specific coordinate system')
+try:
+    parser = argparse.ArgumentParser(description='A script to generate OSM tiles in a specific coordinate system')
 
-parser.add_argument("projection", help="The Proj4 projection to use")
-parser.add_argument("mapnikconfig", help="The mapnik config file to be used for the rendering")
-parser.add_argument("extractarea", help="The extract area as a WKT polygon (coordinates in the destination coordinate system)")
-parser.add_argument("zoomlevels", type=int, nargs='+', help="The zoom levels to render")
-parser.add_argument("--threads", type=int, default=2, help="The number of threads to start rendering with")
-parser.add_argument("--fullClipBelowZoom", type=int, default=-1, help="If specified, the script will only perform a full clip based on the extract polygon below this zoom level, so zoom levels above this will receive the whole extent of the extract region")
-parser.add_argument("--paddigits", type=int, default=8, help="The number of digits to pad coordinates written to file and directory names")
-parser.add_argument("--imgsize", type=int, default=500, help="The resulting image size in pixels")
-parser.add_argument("--basedir", help="The base directory to generate the tiles into")
-parser.add_argument("--mincoordx", type=int, help="The x coordinate to start generating tiles from in the destination coordinate system. If not specified, will be inferred from the extent of the polygon extract region")
-parser.add_argument("--mincoordy", type=int, help="The y coordinate to start generating tiles from in the destination coordinate system. If not specified, will be inferred from the extent of the polygon extract region")
-parser.add_argument("--maxcoordx", type=int, help="The x coordinate to end generating tiles from in the destination coordinate system. If not specified, will be inferred from the extent of the polygon extract region")
-parser.add_argument("--maxcoordy", type=int, help="The y coordinate to end generating tiles from in the destination coordinate system. If not specified, will be inferred from the extent of the polygon extract region")
-parser.add_argument("--coordsincrementx", type=int, default=-1, help="The x coordinate increment to use. If not specified, will be inferred from the extent of the polygon extract region divided by the square of the zoom level (so zoom 0 is the full extent, 1 is divided into half in both direction etc")
-parser.add_argument("--coordsincrementy", type=int, default=-1, help="The y coordinate increment to use. If not specified, will be inferred from the extent of the polygon extract region divided by the square of the zoom level (so zoom 0 is the full extent, 1 is divided into half in both direction etc")
-parser.add_argument("--flatdirectorystructure", action='store_true', help="Whether to generate the tiles in a flat directory or use the x coordinate to split them into seperate directories")
-parser.add_argument("--scale", type=int, default=1, help="How much to scale the rendering of the data. The OSM style sheet works for Global Mercator but for other projections you may have to scale this up a little to avoid having too much detail in the tiles generated")
+    parser.add_argument("projection", help="The Proj4 projection to use")
+    parser.add_argument("mapnikconfig", help="The mapnik config file to be used for the rendering")
+    parser.add_argument("extractarea", help="The extract area as a WKT polygon (coordinates in the destination coordinate system)")
+    parser.add_argument("zoomlevels", type=int, nargs='+', help="The zoom levels to render")
+    parser.add_argument("--threads", type=int, default=2, help="The number of threads to start rendering with")
+    parser.add_argument("--fullClipBelowZoom", type=int, default=-1, help="If specified, the script will only perform a full clip based on the extract polygon below this zoom level, so zoom levels above this will receive the whole extent of the extract region")
+    parser.add_argument("--paddigits", type=int, default=8, help="The number of digits to pad coordinates written to file and directory names")
+    parser.add_argument("--imgsize", type=int, default=500, help="The resulting image size in pixels")
+    parser.add_argument("--basedir", help="The base directory to generate the tiles into")
+    parser.add_argument("--mincoordx", type=int, help="The x coordinate to start generating tiles from in the destination coordinate system. If not specified, will be inferred from the extent of the polygon extract region")
+    parser.add_argument("--mincoordy", type=int, help="The y coordinate to start generating tiles from in the destination coordinate system. If not specified, will be inferred from the extent of the polygon extract region")
+    parser.add_argument("--maxcoordx", type=int, help="The x coordinate to end generating tiles from in the destination coordinate system. If not specified, will be inferred from the extent of the polygon extract region")
+    parser.add_argument("--maxcoordy", type=int, help="The y coordinate to end generating tiles from in the destination coordinate system. If not specified, will be inferred from the extent of the polygon extract region")
+    parser.add_argument("--coordsincrementx", type=int, default=-1, help="The x coordinate increment to use. If not specified, will be inferred from the extent of the polygon extract region divided by the square of the zoom level (so zoom 0 is the full extent, 1 is divided into half in both direction etc")
+    parser.add_argument("--coordsincrementy", type=int, default=-1, help="The y coordinate increment to use. If not specified, will be inferred from the extent of the polygon extract region divided by the square of the zoom level (so zoom 0 is the full extent, 1 is divided into half in both direction etc")
+    parser.add_argument("--flatdirectorystructure", action='store_true', help="Whether to generate the tiles in a flat directory or use the x coordinate to split them into seperate directories")
+    parser.add_argument("--scale", type=int, default=1, help="How much to scale the rendering of the data. The OSM style sheet works for Global Mercator but for other projections you may have to scale this up a little to avoid having too much detail in the tiles generated")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-projection = args.projection
-mapnikconfig = args.mapnikconfig
-imgsize = args.imgsize
-paddigits = args.paddigits
-numthreads = args.threads
-maxQueueSize = 500000
-fullClipBelowZoom = args.fullClipBelowZoom
-extractAreaGeom = ogr.CreateGeometryFromWkt(args.extractarea)
-coordsincrementx = args.coordsincrementx
-coordsincrementy = args.coordsincrementy
-flatDirectoryStructure = args.flatdirectorystructure
-scale = args.scale
+    projection = args.projection
+    mapnikconfig = args.mapnikconfig
+    imgsize = args.imgsize
+    paddigits = args.paddigits
+    numthreads = args.threads
+    maxQueueSize = 500000
+    fullClipBelowZoom = args.fullClipBelowZoom
+    extractAreaGeom = ogr.CreateGeometryFromWkt(args.extractarea)
+    coordsincrementx = args.coordsincrementx
+    coordsincrementy = args.coordsincrementy
+    flatDirectoryStructure = args.flatdirectorystructure
+    scale = args.scale
 
-if args.mincoordx is not None and args.mincoordy is not None and args.maxcoordx is not None and args.maxcoordy is not None:
-    print "Using min and max coordinates from arguments"
-    mincoords = args.mincoordx, args.mincoordy
-    maxcoords = args.maxcoordx, args.maxcoordy
-else:
-    print "Deriving min and max coordinates from extract area extent"
-    extractAreaEnvelope = extractAreaGeom.GetEnvelope()
-    mincoords = (int(extractAreaEnvelope[0]), int(extractAreaEnvelope[2]))
-    maxcoords = (int(extractAreaEnvelope[1])+1, int(extractAreaEnvelope[3])+1)
-
-if args.basedir is not None:
-    basedir = args.basedir
-else:
-    basedir = ""
-
-# A queue to put tiles in for rendering on threads
-queue = Queue()
-
-for zoom in args.zoomlevels:
-
-    print "About to render zoom level: " + str(zoom)
-
-    maxtilesize = max(maxcoords[0] - mincoords[0], maxcoords[1] - mincoords[1])
-
-    print "Max tile size: " + str(maxtilesize)
-
-    if coordsincrementx != -1 and coordsincrementy != -1:
-        xcoordsincrement = coordsincrementx
-        ycoordsincrement = coordsincrementy
+    if args.mincoordx is not None and args.mincoordy is not None and args.maxcoordx is not None and args.maxcoordy is not None:
+        print "Using min and max coordinates from arguments"
+        mincoords = args.mincoordx, args.mincoordy
+        maxcoords = args.maxcoordx, args.maxcoordy
     else:
-        xcoordsincrement = maxtilesize / math.pow(2, zoom)
-        ycoordsincrement = xcoordsincrement
+        print "Deriving min and max coordinates from extract area extent"
+        extractAreaEnvelope = extractAreaGeom.GetEnvelope()
+        mincoords = (int(extractAreaEnvelope[0]), int(extractAreaEnvelope[2]))
+        maxcoords = (int(extractAreaEnvelope[1])+1, int(extractAreaEnvelope[3])+1)
 
-    print "Coordinate increment for zoom level " + str(zoom) + " is " + str(xcoordsincrement)
+    if args.basedir is not None:
+        basedir = args.basedir
+    else:
+        basedir = ""
 
-    x = mincoords[0]
-    y = mincoords[1]
+    # A queue to put tiles in for rendering on threads
+    queue = Queue()
 
+    for zoom in args.zoomlevels:
 
-    for i in range(numthreads):
-        t = ThreadedTileGenerator(queue, i, basedir, projection, mapnikconfig, extractAreaGeom, fullClipBelowZoom, flatDirectoryStructure, scale)
-        t.daemon = True
-        t.start()
+        print "About to render zoom level: " + str(zoom)
 
-    while (y < maxcoords[1]):
-        while (x < maxcoords[0]):
+        maxtilesize = max(maxcoords[0] - mincoords[0], maxcoords[1] - mincoords[1])
 
-            # If the queue gets too big then hang on until the rendering threads have a chance to pick some of these off
-            while (queue.qsize() > maxQueueSize):
-                time.sleep(10)
-            # Truncate all coordinates to an integer when they are passed to the render method
-            queue.put(TileParams(int(x),int(y),int(x+xcoordsincrement),int(y+ycoordsincrement),imgsize,zoom,paddigits))
-            x = x+xcoordsincrement
-        y = y+ycoordsincrement
+        print "Max tile size: " + str(maxtilesize)
+
+        if coordsincrementx != -1 and coordsincrementy != -1:
+            xcoordsincrement = coordsincrementx
+            ycoordsincrement = coordsincrementy
+        else:
+            xcoordsincrement = maxtilesize / math.pow(2, zoom)
+            ycoordsincrement = xcoordsincrement
+
+        print "Coordinate increment for zoom level " + str(zoom) + " is " + str(xcoordsincrement)
+
         x = mincoords[0]
+        y = mincoords[1]
 
-    # Blocks until all items in the queue are processed
-    queue.join()
+
+        for i in range(numthreads):
+            t = ThreadedTileGenerator(queue, i, basedir, projection, mapnikconfig, extractAreaGeom, fullClipBelowZoom, flatDirectoryStructure, scale)
+            t.daemon = True
+            t.start()
+
+        while (y < maxcoords[1]):
+            while (x < maxcoords[0]):
+
+                # If the queue gets too big then hang on until the rendering threads have a chance to pick some of these off
+                while (queue.qsize() > maxQueueSize):
+                    time.sleep(10)
+                # Truncate all coordinates to an integer when they are passed to the render method
+                queue.put(TileParams(int(x),int(y),int(x+xcoordsincrement),int(y+ycoordsincrement),imgsize,zoom,paddigits))
+                x = x+xcoordsincrement
+            y = y+ycoordsincrement
+            x = mincoords[0]
+
+        # Blocks until all items in the queue are processed
+        queue.join()
+except KeyboardInterrupt:
+    print "Requesting threads to shutdown..."
+    shutdownRequested = True
+    raise
 
 
 
