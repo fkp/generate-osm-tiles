2759ec8d2ae5a335986358d594cc7dc3a9089cdb
fkp <no@spam.com>
2015-05-24 05:59:50 +0100

    Fix

--- planet/import_uk.sh
+++ planet/import_uk.sh
@@ -1,3 +1,3 @@
-rm great-britain-latest.osm.pbf
+rm british-isles-latest.osm.pbf
 wget http://download.geofabrik.de/europe/british-isles-latest.osm.pbf
 osm2pgsql --slim -C 1000 -E EPSG:4326 british-isles-latest.osm.pbf
