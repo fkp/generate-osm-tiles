21ab9e5b5fa6283820f0e8d7cdf328e7ff4e6177
fkp <no@spam.com>
2015-07-05 19:26:46 +0100

    Fixes to clip extent and expand out the spatial extent

--- mapnik/reprojectCoast_2260.sh
+++ mapnik/reprojectCoast_2260.sh
@@ -6,7 +6,7 @@
 source=/etc/mapnik-osm-carto-data/data/
 
 # THESE COORDINATES NEED TO BE IN GLOBAL MERCATOR
-extent="-8844696.57158 4594657.77135 -8154152.70061 6026245.6093"
+extent="-9544696.57158 4094657.77135 -7054152.70061 6526245.6093"
 srs=2260ft
 proj4="+proj=tmerc +lat_0=38.83333333333334 +lon_0=-74.5 +k=0.9999 +x_0=150000 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs "
 
@@ -28,6 +28,7 @@ for fullPath in \
   "${source}ne_10m_populated_places/ne_10m_populated_places.shp" \
   "${source}ne_10m_populated_places/ne_10m_populated_places_fixed.shp" \
   "${source}land-polygons-split-3857/land_polygons.shp" \
+  "${source}simplified-land-polygons-complete-3857/simplified_land_polygons.shp" \
   "${source}ne_110m_admin_0_boundary_lines_land/ne_110m_admin_0_boundary_lines_land.shp" \
   "${source}world_boundaries/builtup_area.shp" \
   "${source}world_boundaries/world_boundaries_m.shp" \
@@ -43,7 +44,7 @@ do
   echo Converting: $file
 
   ogr2ogr -f "ESRI Shapefile" -s_srs EPSG:2260 -t_srs "${proj4}" \
-     -spat ${extent} ${destinationFullPath} ${fullPath}
+     -clipsrc ${extent} ${destinationFullPath} ${fullPath}
   
 done
 
