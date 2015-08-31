09cb70bee320c06b52146130d438c3e8c1392b9e
fkp <fraser.a.kirkpatrick@gmail.com>
2015-08-31 21:35:53 +0100

    reproject rework

---
+++ mapnik/reprojectCoast.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Reproject the various shapefiles into a target srs
+
+# location of shape files
+source=/etc/mapnik-osm-carto-data/data/
+
+# THESE COORDINATES NEED TO BE IN GLOBAL MERCATOR
+if [ "$#" -ne 3 ]
+then
+  echo "Expected: <extent> <srs> <proj4>"
+  exit 1
+fi
+
+extent=$1
+srs=$2
+proj4=$3
+
+echo "Using:"
+echo " extent: " $1
+echo " srs: " $2
+echo " proj4: " $3
+
+function DeleteShapefile
+{
+  for extension in .sbf .prj .shp .shx
+  do
+    fileToDelete=${1/.shp/$extension}
+    if [ -f $fileToDelete ]
+    then
+      rm $fileToDelete
+    fi
+  done
+}
+
+echo "Extract for ${srs}"
+
+for fullPath in \
+  "${source}ne_10m_populated_places/ne_10m_populated_places.shp" \
+  "${source}ne_10m_populated_places/ne_10m_populated_places_fixed.shp" \
+  "${source}land-polygons-split-3857/land_polygons.shp" \
+  "${source}simplified-land-polygons-complete-3857/simplified_land_polygons.shp" \
+  "${source}ne_110m_admin_0_boundary_lines_land/ne_110m_admin_0_boundary_lines_land.shp" \
+  "${source}world_boundaries/builtup_area.shp" \
+  "${source}world_boundaries/world_boundaries_m.shp" \
+  "${source}world_boundaries/places.shp"
+do
+  file=$(basename $fullPath)
+  dir=$(dirname $fullPath)
+
+  destinationFullPath=$dir/${file/.shp/_$srs.shp}
+  
+  DeleteShapefile ${destinationFullPath}
+
+  echo Converting: $file
+
+  ogr2ogr -f "ESRI Shapefile" -s_srs EPSG:2260 -t_srs "${proj4}" \
+     -clipsrc ${extent} ${destinationFullPath} ${fullPath}
+  
+done
+
--- mapnik/reprojectCoast_2260.sh
+++ mapnik/reprojectCoast_2260.sh
@@ -1,50 +1,6 @@
 #!/bin/bash
 
 # Reproject the various shapefiles into a target srs
+./reprojectCoast.sh "-9544696.57158 4094657.77135 -7054152.70061 6526245.6093" 2260ft "+proj=tmerc +lat_0=38.83333333333334 +lon_0=-74.5 +k=0.9999 +x_0=150000 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs "
 
-# location of shape files
-source=/etc/mapnik-osm-carto-data/data/
-
-# THESE COORDINATES NEED TO BE IN GLOBAL MERCATOR
-extent="-9544696.57158 4094657.77135 -7054152.70061 6526245.6093"
-srs=2260ft
-proj4="+proj=tmerc +lat_0=38.83333333333334 +lon_0=-74.5 +k=0.9999 +x_0=150000 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs "
-
-function DeleteShapefile
-{
-  for extension in .sbf .prj .shp .shx
-  do
-    fileToDelete=${1/.shp/$extension}
-    if [ -f $fileToDelete ]
-    then
-      rm $fileToDelete
-    fi
-  done
-}
-
-echo "Extract for ${srs}"
-
-for fullPath in \
-  "${source}ne_10m_populated_places/ne_10m_populated_places.shp" \
-  "${source}ne_10m_populated_places/ne_10m_populated_places_fixed.shp" \
-  "${source}land-polygons-split-3857/land_polygons.shp" \
-  "${source}simplified-land-polygons-complete-3857/simplified_land_polygons.shp" \
-  "${source}ne_110m_admin_0_boundary_lines_land/ne_110m_admin_0_boundary_lines_land.shp" \
-  "${source}world_boundaries/builtup_area.shp" \
-  "${source}world_boundaries/world_boundaries_m.shp" \
-  "${source}world_boundaries/places.shp"
-do
-  file=$(basename $fullPath)
-  dir=$(dirname $fullPath)
-
-  destinationFullPath=$dir/${file/.shp/_$srs.shp}
-  
-  DeleteShapefile ${destinationFullPath}
-
-  echo Converting: $file
-
-  ogr2ogr -f "ESRI Shapefile" -s_srs EPSG:2260 -t_srs "${proj4}" \
-     -clipsrc ${extent} ${destinationFullPath} ${fullPath}
-  
-done
 
