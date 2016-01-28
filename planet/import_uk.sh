8ac668f2d632f4f2a1a0390fbe07d996ee1b1aae
fkp <no@spam.com>
2016-01-28 20:31:23 +0000

    Updates

--- planet/import_uk.sh
+++ planet/import_uk.sh
@@ -1,3 +1,4 @@
 rm british-isles-latest.osm.pbf
 wget http://download.geofabrik.de/europe/british-isles-latest.osm.pbf
 osm2pgsql --slim -C 1000 -E EPSG:4326 british-isles-latest.osm.pbf
+#osm2pgsql --slim -C 1500 --number-processes 4 -E EPSG:4326 british0isles-latest.osm.pbf
