e16f9d8b67ca69033555343c1541c090dae6fc57
fkp <fkp@users.noreply.github.com>
2018-07-05 17:22:31 +0100

    Tweak

--- run_26910.sh
+++ run_26910.sh
@@ -5,6 +5,5 @@ mapnikconfig="/etc/mapnik-osm-carto-data/osm_26910.xml"
 extentWKT26910m="POLYGON ((494000 5994000, 538000 5994000, 538000 5954000, 494000 5954000, 494000 5994000))"
 basedir=CA26910
 
-#./GenerateTiles.py --flatdirectorystructure --basedir ${basedir} --imgsize 4000 --scale 4 "${projection26910m}" ${mapnikconfig} "${extentWKT26910m}" 0 1 2 3 4 5 6 7 8 9
-./GenerateTiles.py --flatdirectorystructure --basedir ${basedir} --imgsize 4000 --scale 4 "${projection26910m}" ${mapnikconfig} "${extentWKT26910m}" 0 1 2 3 4
+./GenerateTiles.py --flatdirectorystructure --basedir ${basedir} --imgsize 4000 --scale 4 "${projection26910m}" ${mapnikconfig} "${extentWKT26910m}" 0 1 2 3 4 5 6
 
